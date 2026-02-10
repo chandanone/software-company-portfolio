b91919daf36ad474949b726a258390725b0d1418
chandanone <chandan868@gmail.com>
2026-02-10 12:38:35 +0530

    college name case

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -59,12 +59,12 @@ export function Footer() {
                             <div className="flex items-start space-x-2">
                                 <MapPin className="w-3.5 h-3.5 sm:w-4 sm:h-4 text-[#00aaff] shrink-0 mt-0.5" />
                                 <span className="leading-tight">Tripund Technologies Pvt. Ltd.<br />
-                                    NEAR RNTI COLLEGE HATIA, RANCHI 834003
+                                    Near RNTI College Hatia, Ranchi 834003
                                 </span>
                             </div>
                             <div className="flex items-center space-x-2">
                                 <Phone className="w-3.5 h-3.5 sm:w-4 sm:h-4 text-[#00aaff] shrink-0" />
-                                <span>+91 8892929292</span>
+                                <span>+91 XXXXXXXXXX</span>
                             </div>
                             <div className="flex items-center space-x-2">
                                 <Mail className="w-3.5 h-3.5 sm:w-4 sm:h-4 text-[#00aaff] shrink-0" />
